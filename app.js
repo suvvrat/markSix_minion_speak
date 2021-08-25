6eb2d1cd71732201ed9c401f181e2429a6e0dd9b
Suvrat Verma <75042545+suvvrat@users.noreply.github.com>
2021-08-25 17:59:35 +0530

    finished with markSix project

---
+++ app.js
@@ -0,0 +1,20 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+btnTranslate.addEventListener("click", clickHandler);
+function clickHandler(){
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response=>response.json())
+        .then(json=>{
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+}
+    
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
+}
